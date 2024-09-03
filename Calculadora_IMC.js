f4e8531d5548e0935681bc660fa5844ff4a930ab
Christian Vinicius Santos <viniciuschristian93@gmail.com>
2024-09-03 12:23:11 -0300

    Subindo o começo da tela

---
+++ Calculadora_IMC.js
@@ -0,0 +1,9 @@
+function Calcular(){
+    let KG = Number(document.getElementById("Kg_input").value)
+    let altura = Number(document.getElementById("Altura_input").value)
+    let resultado = document.getElementById("resultado")
+
+    resultado.innerHTML = `` 
+ 
+    
+}

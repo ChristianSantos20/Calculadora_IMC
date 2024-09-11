3f6bc2c17a8f801ef4b28a7fc89fb5148d520b32
Christian Vinicius Santos <viniciuschristian93@gmail.com>
2024-09-11 13:13:47 -0300

    Subindo

--- Calculadora_IMC.js
+++ Calculadora_IMC.js
@@ -1,9 +1,25 @@
-function Calcular(){
-    let KG = Number(document.getElementById("Kg_input").value)
-    let altura = Number(document.getElementById("Altura_input").value)
-    let resultado = document.getElementById("resultado")
+let formulario_calculos = document.getElementById("Calculos_div");
 
-    resultado.innerHTML = `` 
- 
+
+
+formulario_calculos.addEventListener("submit", function(i){
+    i.preventDefault()
+    let kg = document.getElementById("Kg_input");
+    let Altura = document.getElementById("Altura_input");
+    let resultado = document.getElementById("resultado");
+  
+    if(kg.value === ""){
+        resultado.innerHTML = "Preencha os campos vazios";
+        kg.focus();
+        return; 
+    };
+
+    if(Altura.value === ""){
+        resultado.innerHTML = "Preencha os campos vazios";
+        Altura.focus();
+        return;
+    };
+
+    formulario_calculos.submit();
     
-}
+})

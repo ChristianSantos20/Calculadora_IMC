6690c712bc971f29f65b6d193ec100399566eab3
Christian Vinicius Santos <viniciuschristian93@gmail.com>
2024-10-14 13:46:10 -0300

    Modificações no javascript

--- Calculadora_IMC.js
+++ Calculadora_IMC.js
@@ -1,14 +1,29 @@
 //Mascara do KG
 function KG_mask(k){
-    k=k.replace(/\D/g,"")
-    k=k.replace(/^(\d{2})(\d{2})/,"$1,$2")
-    return k;
+
+    let Kg_valor = k.value
+
+    Kg_valor = Kg_valor.replace(/\D/g,"");
+
+    if(Kg_valor.length === 2){
+       Kg_valor.replace(/^(\d{2})(\d{0,3})/,"$1,$2");
+    }
+    
+    k.value = Kg_valor;
+
 }
 //Mascara da altura
 function Altura_Mask(a){
-    a=a.replace(/\D/g,"");
-    a=a.replace(/^(\d{2})(\d{2})/,"$1.$2");
-    return a;
+
+    let Altura_valor = a.value;
+    Altura_valor = Altura_valor.replace(/\D/g,"");
+
+    if(Altura_valor.length === 1){
+        Altura_valor.replace(/^(\d{1})(\d{0,3})/,"$1.$2")
+    };
+   
+    a.value = Altura_valor;
+    
 } 
 //Função para calcular o IMC
 function Calcular(){
@@ -16,22 +31,29 @@ function Calcular(){
     let KG = Number(document.getElementById("Kg_input").value)
     let Altura = Number(document.getElementById("Altura_input").value)
     let resultado = document.getElementById("resultado")
-     
-    const IMC = KG / (Altura * Altura)
     
-    if(IMC < 18){
-        resultado.innerHTML("Abaixo do peso")
-    } else if(IMC > 18 && IMC < 24){
-        resultado.innerHTML("Peso normal")
-    } else if(IMC > 25 && IMC < 29){
-        resultado.innerHTML("Execesso de peso")
-    } else if(IMC > 29 && IMC < 34){
-        resultado.innerHTML("Obesidade classe I")
-    } else if(IMC > 34 && IMC < 39){
-        resultado.innerHTML("Obesidade classe II")
-    } else {
-        resultado.innerHTML("Obesidade classe III")
-    }
+    if(KG === "" || Altura === ""){
+        alert("Preencha os campos vazios");
+        return;
+    } 
 
-    //return resultado;
+    const IMC = KG / (Altura * Altura);
+
+        if(IMC < 18){
+           resultado.innerHTML("Abaixo do peso")
+        } else if(IMC > 18 && IMC < 24){
+           resultado.innerHTML("Peso normal")
+        } else if(IMC > 25 && IMC < 29){
+           resultado.innerHTML("Execesso de peso")
+        } else if(IMC > 29 && IMC < 34){
+           resultado.innerHTML("Obesidade classe I")
+        } else if(IMC > 34 && IMC < 39){
+           resultado.innerHTML("Obesidade classe II")
+        } else {
+           resultado.innerHTML("Obesidade classe III")
+        }
+
+        return resultado;
+        
+
 }
